3b0e0ee56bcd8d3cb09a032990c035c420802bcc
graanjith004 <raanjith004@gmail.com>
2026-02-19 12:56:56 +0530

    second commit

---
+++ ReverseArr.js
@@ -0,0 +1,15 @@
+// Reverse an array in JavaScript
+
+let marks=[98,87,139,456,45];
+let result=[];
+
+function reverseArr(arrVal){
+for(let i=arrVal.length-1;i>=0;i--){
+result.push(arrVal[i]);
+}
+return result;
+}
+
+let output=reverseArr(marks)
+console.log("The real reverse array is", output);
+console.log(`The reverse array is [${output}]`);
